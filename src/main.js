e7a5617793e9ca37b415a31fd417405631f40149
avagranta <1814048443@qq.com>
2022-08-09 20:04:53 +0800

    feat: 登录页面搭建、图标库及图标组件处理

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
+import installIcons from '@/icons'
+import './styles/index.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installIcons(app)
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
