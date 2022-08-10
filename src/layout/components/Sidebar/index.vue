cac88a6f3b9d36bed473e56d0960c1e5d156de03
avagranta <1814048443@qq.com>
2022-08-10 21:47:00 +0800

    feat: 菜单栏结构搭建

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="">Sidebar</div>
+  <div class="">
+    <h1>Sidebar</h1>
+    <el-scrollbar>
+      <sidebar-menu></sidebar-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup>
 import {} from 'vue'
+import SidebarMenu from './SidebarMenu.vue'
 </script>
 
 <style lang="scss" scoped></style>
