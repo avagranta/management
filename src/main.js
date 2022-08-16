71b74b05fc1ed76ebd7303a9f9468614df9d7de1
avagranta <1814048443@qq.com>
2022-08-16 13:23:32 +0800

    feat: 拖拽导入和文件选择导入完成

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import installIcons from '@/icons'
+import installFilter from '@/filter'
 import './styles/index.scss'
 import './permission'
 
 const app = createApp(App)
 installIcons(app)
 installElementPlus(app)
+installFilter(app)
 app.use(store).use(router).mount('#app')
--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,7 +1,41 @@
-<template>import</template>
+<template>
+  <upload-excel :onSuccess="onSuccess"></upload-excel>
+</template>
 
-<script>
-export default {}
+<script setup>
+import UploadExcel from '@/components/UploadExcel'
+import { formatDate, USER_RELATIONS } from './utils'
+import { ElMessage } from 'element-plus'
+import { useRouter } from 'vue-router'
+import { userBatchImport } from '@/api/user-manage'
+
+const router = useRouter()
+
+/**
+ * 数据解析成功之后的回调
+ */
+const onSuccess = async ({ header, results }) => {
+  const updateData = generateData(results)
+  await userBatchImport(updateData)
+  ElMessage.success({
+    message: results.length + ' 条员工数据导入成功',
+    type: 'success'
+  })
+  router.push('/user/manage')
+}
+
+/**
+ * 筛选数据
+ */
+const generateData = (results) => {
+  const arr = []
+  results.forEach((item) => {
+    const userInfo = {}
+    Object.keys(item).forEach((key) => {
+      userInfo[USER_RELATIONS[key]] = formatDate(item[key])
+    })
+    arr.push(userInfo)
+  })
+  return arr
+}
 </script>
-
-<style></style>
