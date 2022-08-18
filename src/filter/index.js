56c73c44badfd39a66b25d839f6783ceaf5c495b
avagranta <1814048443@qq.com>
2022-08-18 10:40:14 +0800

    feat: 动态表格实现

--- src/filter/index.js
+++ src/filter/index.js
@@ -1,15 +1,27 @@
 import dayjs from 'dayjs'
+import rt from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
+
+// 相对时间插件
+dayjs.extend(rt)
+function relativeTime(val) {
+  if (!isNaN(val)) {
+    val = parseInt(val)
+  }
+  // console.log(dayjs().to(dayjs(val)))
+  return dayjs().locale('zh-cn').to(dayjs(val))
+}
 
 const dateFilter = (val, format = 'YYYY-MM-DD') => {
   if (!isNaN(val)) {
     val = parseInt(val)
   }
-
   return dayjs(val).format(format)
 }
 
 export default (app) => {
   app.config.globalProperties.$filters = {
-    dateFilter
+    dateFilter,
+    relativeTime
   }
 }
---
+++ src/views/article-ranking/dynamic/DynamicData.js
@@ -0,0 +1,26 @@
+export default () => [
+  {
+    label: '排名',
+    prop: 'ranking'
+  },
+  {
+    label: '标题',
+    prop: 'title'
+  },
+  {
+    label: '作者',
+    prop: 'author'
+  },
+  {
+    label: '发布时间',
+    prop: 'publicDate'
+  },
+  {
+    label: '内容简介',
+    prop: 'desc'
+  },
+  {
+    label: '操作',
+    prop: 'action'
+  }
+]
